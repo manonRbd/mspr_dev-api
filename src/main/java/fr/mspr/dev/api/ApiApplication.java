b3812d707c2bc523c73f0e29fe11b4110bac60dd
Charly85_ <caillon.charly@gmail.com>
2019-05-16 14:27:00 +0200

    Return objects uuid

--- src/main/java/fr/mspr/dev/api/ApiApplication.java
+++ src/main/java/fr/mspr/dev/api/ApiApplication.java
@@ -1,14 +1,20 @@
 package fr.mspr.dev.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -19,6 +25,22 @@ public class ApiApplication {
 			http
 					.httpBasic().disable()
 					.csrf().disable();
+		}
+	}
+
+
+	@Configuration
+	public class RestConfiguration implements RepositoryRestConfigurer {
+
+		@Autowired
+		private EntityManager entityManager;
+
+		@Override
+		public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+			config.exposeIdsFor(
+					entityManager.getMetamodel().getEntities().stream()
+							.map(Type::getJavaType)
+							.toArray(Class[]::new));
 		}
 	}
 
