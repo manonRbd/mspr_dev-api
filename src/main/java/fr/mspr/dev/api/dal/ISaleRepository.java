dc2edcd3bae20b9e929e50f262553e4f4c509d9f
Charly85_ <caillon.charly@gmail.com>
2019-05-21 11:48:57 +0200

    Sales by repository

--- src/main/java/fr/mspr/dev/api/dal/ISaleRepository.java
+++ src/main/java/fr/mspr/dev/api/dal/ISaleRepository.java
@@ -2,8 +2,11 @@ package fr.mspr.dev.api.dal;
 
 import fr.mspr.dev.api.domain.Sale;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-//@CrossOrigin(origins = "*")
-//public interface ISaleRepository extends CrudRepository<Sale, String> {
-//}
+@CrossOrigin(origins = "*")
+public interface ISaleRepository extends CrudRepository<Sale, String> {
+    Iterable<Sale> findAllByPractitionerUuid(@Param("uuid_practitioner") String uuid_practitioner);
+    Iterable<Sale> findAllByProductUuid(@Param("uuid_product") String uuid_product);
+}
