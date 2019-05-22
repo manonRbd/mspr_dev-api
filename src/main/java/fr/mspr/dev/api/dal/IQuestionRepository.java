589e5666e6a617a43a7b030fb1b7713aff4d014d
Charly85_ <caillon.charly@gmail.com>
2019-05-22 16:28:32 +0200

    Get question fy form and position

--- src/main/java/fr/mspr/dev/api/dal/IQuestionRepository.java
+++ src/main/java/fr/mspr/dev/api/dal/IQuestionRepository.java
@@ -8,4 +8,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "*")
 public interface IQuestionRepository extends CrudRepository<Question, String> {
+    public Question findByFormUuidAndPosition(@Param("form_uuid") String form_uuid, @Param("position") Integer position);
 }
